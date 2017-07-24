a01fcfb1ff191848f7976a1088111cf6eb7dca43
Deirdre Hyde <deirdre@platinumperformance.com>
2017-07-24 12:16:38 -0700

    Update side nav, can scroll on playlist index

--- Spotify/frontend/reducers/playlist_reducer.js
+++ Spotify/frontend/reducers/playlist_reducer.js
@@ -7,7 +7,7 @@ const PlaylistReducer = (state = {}, action) => {
     case RECEIVE_PLAYLISTS:
       return Object.assign({}, action.playlists);
     case RECEIVE_PLAYLIST:
-      return Object.assign({}, {[action.playlist.id]: action.playlist});
+      return Object.assign({}, state, {[action.playlist.id]: action.playlist});
     default:
       return state;
   }
