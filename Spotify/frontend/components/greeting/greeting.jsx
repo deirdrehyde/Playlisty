cd34fa444bb0d150a7c3567b9c59d067865d2c68
Deirdre Hyde <deirdre@platinumperformance.com>
2017-07-24 13:29:10 -0700

    Side nav enhancements

--- Spotify/frontend/components/greeting/greeting.jsx
+++ Spotify/frontend/components/greeting/greeting.jsx
@@ -9,6 +9,7 @@ const greetingMessage = (currentUser, logout) => (
   <div className="greeting">
     <div className="top-side-nav">
       <img className="logo-small" src="http://res.cloudinary.com/douzdapki/image/upload/v1500486429/Refresh_xdfvbr.png" />
+      <Link to='/search' className='search-nav'>Search</Link>
       <Link to='/playlists' className='playlist-nav'>Your Music</Link>
     </div>
     <div className="bottom-side-nav">
