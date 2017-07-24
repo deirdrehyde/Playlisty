2cd5d7ad679039c86dcd70f0171cd7172776a4f5
Deirdre Hyde <deirdre@platinumperformance.com>
2017-07-24 16:40:35 -0700

    Add seeds

--- Spotify/db/seeds.rb
+++ Spotify/db/seeds.rb
@@ -7,5 +7,12 @@
 #   Character.create(name: 'Luke', movie: movies.first)
 
 guest = User.create(username: "Guest", password: "password")
+guest1 = User.create(username: "Playlisty", password: "password123!")
 playlist1 = Playlist.create(name: "Top Hits", creator_id: 1)
 playlist2 = Playlist.create(name: "Classic Rock", creator_id: 1)
+playlist3 = Playlist.create(name: "Hip Hop", creator_id: 2)
+playlist4 = Playlist.create(name: "Beach Vibes", creator_id: 2)
+playlist5 = Playlist.create(name: "Hot Country", creator_id: 2)
+playlist6 = Playlist.create(name: "Bluegrass", creator_id: 2)
+playlist7 = Playlist.create(name: "Classical", creator_id: 2)
+playlist8 = Playlist.create(name: "Jazz", creator_id: 2)
