c3784df1f3e8f6f0cd9a9fb865de7a5d0c8c5289
Deirdre Hyde <deirdre@platinumperformance.com>
2017-07-28 14:03:54 -0700

    define plugins

--- Spotify/webpack.config.js
+++ Spotify/webpack.config.js
@@ -1,5 +1,7 @@
 const path = require('path');
 const webpack = require('webpack');
+const plugins = []; // if using any plugins for both dev and production
+const devPlugins = []; // if using any plugins for development
 
 const prodPlugins = [
   new webpack.DefinePlugin({
